616665d6dd48f677fada15daf9f314fd750da78b
atodorovska <ana5todorovska@gmail.com>
2021-02-21 16:57:49 +0100

    init commit

---
+++ web/src/main/java/mk/ukim/finki/emt/eshop/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package mk.ukim.finki.emt.eshop.repository;
+
+import mk.ukim.finki.emt.eshop.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByName(String name);
+    void deleteByName(String name);
+}
